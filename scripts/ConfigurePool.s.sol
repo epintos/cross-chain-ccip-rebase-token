f9844c3f56a02c9220a8774f03f7fe8c87f94a92
Esteban Pintos <esteban.pintos@wetransfer.com>
2025-01-24 11:13:14 +0100

    Adds ConfigurePoolScript

---
+++ scripts/ConfigurePool.s.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import { Script } from "forge-std/Script.sol";
+import { TokenPool } from "@ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
+import { RateLimiter } from "@ccip/contracts/src/v0.8/ccip/libraries/RateLimiter.sol";
+
+contract ConfigurePoolScript is Script {
+    function run(
+        address localPool,
+        uint64 removeChainSelector,
+        address remotePool,
+        address remoteToken,
+        bool outbountRateLimiterIsEnabled,
+        uint128 outboundRateLimiterCapacitiy,
+        uint128 outboundRateLimiterRate,
+        bool inboundRateLimiterIsEnabled,
+        uint128 inboundRateLimiterCapacitiy,
+        uint128 inboundRateLimiterRate
+    )
+        public
+    {
+        vm.startBroadcast();
+        TokenPool.ChainUpdate[] memory chainsToAdd = new TokenPool.ChainUpdate[](1);
+        chainsToAdd[0] = TokenPool.ChainUpdate({
+            remoteChainSelector: remoteChainSelector,
+            allowed: true,
+            remotePoolAddress: abi.encode(remotePool),
+            remoteTokenAddress: abi.encode(remoteToken),
+            outboundRateLimiterConfig: RateLimiter.Config({
+                isEnabled: outbountRateLimiterIsEnabled,
+                capacity: outboundRateLimiterCapacitiy,
+                rate: outboundRateLimiterRate
+            }),
+            inboundRateLimiterConfig: RateLimiter.Config({
+                isEnabled: inboundRateLimiterIsEnabled,
+                capacity: inboundRateLimiterCapacitiy,
+                rate: inboundRateLimiterRate
+            })
+        });
+        TokenPool(localPool).applyChainUpdates(chainsToAdd);
+        vm.stopBroadcast();
+    }
+}
